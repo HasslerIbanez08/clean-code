94f3845065580a31e5d78b31fa50bbdb4195f859
HasslerIbanez08 <77356748+HasslerIbanez08@users.noreply.github.com>
2021-02-02 14:26:57 +0100

    exception

--- ReactiveProgramingExample/src/main/java/MainCompletableFutureExercise.java
+++ ReactiveProgramingExample/src/main/java/MainCompletableFutureExercise.java
@@ -11,8 +11,20 @@ public class MainCompletableFutureExercise {
         System.out.println("la resta del resultado de la suma es : "+completableSubtraction.get());
         CompletableFuture<Integer> completableMultiply= completableSubtraction.thenApplyAsync((resultado)-> calculator.multiply(resultado,resultado));
         System.out.println("la multiplicacion del resultado de la resta es : "+completableMultiply.get());
-        CompletableFuture<Integer> completableDivide= completableSubtraction.thenApplyAsync((resultado)-> calculator.divide(resultado,resultado));
-        System.out.println("la divicion del resultado de la multiplicacion es : "+completableMultiply.get());
+        CompletableFuture<Integer> completableDivide= completableSubtraction.thenApplyAsync((resultado)-> {
+            if(resultado==0){
+                throw  new RuntimeException("Error en la operacion resultado es igual "+ resultado);
+            }
+            return calculator.divide(resultado,resultado);
+        }).handle((success,error)->{
+            return  success!=null?success:0;
+        });
+        if(completableDivide.get()!=0){
+            System.out.println("la divicion del resultado de la multiplicacion es : "+completableDivide.get());
+        }else {
+            System.out.println("Error en la operacion resultado es igual 0");
+        }
+
         CompletableFuture<String> futureCopyright = CompletableFuture.supplyAsync(()->"----- Copyright-----")
                 .thenApply((mensaje)-> mensaje + " Hassler ")
                 .thenApply((mensaje)-> mensaje + "ibañez");
